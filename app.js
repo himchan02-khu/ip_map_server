fc8d94f47c35366ae2eb22d71185805006f25d2a
추힘찬 <himchan02@khu.ac.kr>
2024-06-09 05:07:45 +0900

    Delete: Annotations

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express();
 const port = 4242;
 const token = "0d72321f5ba3d2";
 const url = require("url");
-const apiHelper = require('./googlemap');
+const apiHelper = require('./map');
 
 // const http = require("http");
 // const {router} = require("express/lib/application");
@@ -17,21 +17,13 @@ app.listen(port, () => {
     console.log(`How to get one address: http://localhost:${port}/address?lat={lat}&lon={lon}`);
     console.log(`How to get two address: http://localhost:${port}/addresses?lat1={lat1}&lon1={lon1}&lat2={lat2}&lon2={lon2}`);
 });
-// Login page route
+
 app.get('/search', async (req, res) => {
     try {
         const query = url.parse(req.url, true).query;
         console.log('request: ', query);
         const response = await ipinfo(query.ip);
-        // const info = await Promise.all(
-        //     response.data.map(async (item) => {
-        //         ip: item.ip;
-        //         city: item.city;
-        //         country: item.country;
-        //         region: item.region;
-        //
-        //     })
-        // )
+
         if (response.ip == null)
             return res.status(404).send("No ip found.");
         return res.json(response);
@@ -46,7 +38,7 @@ app.get('/address', async (req, res) => {
     const { lat, lon } = req.query;
     try {
         const address = await apiHelper.fetchAddress(lat, lon);
-        console.log(address);
+        console.log(`Address : ${address}`);
         res.json({ address });
     } catch (error) {
         res.status(500).json({ error: error.message });
@@ -57,7 +49,7 @@ app.get('/addresses', async (req, res) => {
     const { lat1, lon1, lat2, lon2 } = req.query;
     try {
         const addresses = await apiHelper.fetchAddresses(lat1, lon1, lat2, lon2);
-        console.log(addresses);
+        console.log(`Two Address : {'${addresses.address1}', '${addresses.address2}'}`);
         res.json(addresses);
     } catch (error) {
         res.status(500).json({ error: error.message });
@@ -104,37 +96,3 @@ async function ipinfo(IPaddress) {
         return {};
     }
 }
-//
-//
-// function initMap() {
-//     const geocoder = new google.maps.Geocoder();
-//     const map = document.querySelector('gmp-map').innerMap;
-//     const infoWindow = new google.maps.InfoWindow();
-//
-//     document.getElementById('submit').addEventListener('click', () => {
-//         geocodeLatLng(geocoder, map, infoWindow);
-//     });
-// }
-//
-// async function geocodeLatLng(geocoder, map, infoWindow) {
-//     const input = document.getElementById('latlng').value;
-//     const latlngStr = input.split(',', 2);
-//     const latlng = {
-//         lat: parseFloat(latlngStr[0]),
-//         lng: parseFloat(latlngStr[1]),
-//     };
-//
-//     try {
-//         const response = await geocoder.geocode({location: latlng});
-//         const marker = document.querySelector('gmp-advanced-marker');
-//
-//         map.setZoom(11);
-//         marker.position = latlng;
-//         infoWindow.setContent(response.results[0].formatted_address);
-//         infoWindow.open({anchor: marker});
-//     } catch (e) {
-//         window.alert(`Geocoder failed due to: ${e}`);
-//     }
-// }
-//
-// window.initMap = initMap;
